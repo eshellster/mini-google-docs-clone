7a6779fb152fb0af9f93c8f7cfe6d9e743130717
eshell <eshellster@gmail.com>
2020-11-20 22:25:52 +0900

    최소화

--- src/SyncingEditor.tsx
+++ src/SyncingEditor.tsx
@@ -1,43 +1,6 @@
-import React, { useCallback, useMemo, useState } from "react";
-import { createEditor, Editor, Transforms, Text, Node } from "slate";
+import React, { useMemo, useState } from "react";
+import { createEditor, Node } from "slate";
 import { Slate, Editable, withReact } from "slate-react";
-
-const CustomEditor = {
-  isBoldMarkActive(editor: any) {
-    const [match] = Editor.nodes(editor, {
-      match: (n) => n.bold === true,
-      universal: true,
-    });
-
-    return !!match;
-  },
-
-  isCodeBlockActive(editor: any) {
-    const [match] = Editor.nodes(editor, {
-      match: (n) => n.type === "code",
-    });
-
-    return !!match;
-  },
-
-  toggleBoldMark(editor: any) {
-    const isActive = CustomEditor.isBoldMarkActive(editor);
-    Transforms.setNodes(
-      editor,
-      { bold: isActive ? null : true },
-      { match: (n) => Text.isText(n), split: true }
-    );
-  },
-
-  toggleCodeBlock(editor: any) {
-    const isActive = CustomEditor.isCodeBlockActive(editor);
-    Transforms.setNodes(
-      editor,
-      { type: isActive ? null : "code" },
-      { match: (n) => Editor.isBlock(editor, n) }
-    );
-  },
-};
 
 export const SyncingEditor = () => {
   const editor = useMemo(() => withReact(createEditor()), []);
@@ -48,86 +11,9 @@ export const SyncingEditor = () => {
     },
   ]);
 
-  const renderElement = useCallback((props) => {
-    switch (props.element.type) {
-      case "code":
-        return <CodeElement {...props} />;
-      default:
-        return <DefaultElement {...props} />;
-    }
-  }, []);
-
-  const renderLeaf = useCallback((props) => {
-    return <Leaf {...props} />;
-  }, []);
-
   return (
-    // 동일한 메서드를 호출하는 버튼이 있는 툴바를 추가합니다.
     <Slate editor={editor} value={value} onChange={(value) => setValue(value)}>
-      <div>
-        <button
-          onMouseDown={(event) => {
-            event.preventDefault();
-            CustomEditor.toggleBoldMark(editor);
-          }}
-        >
-          Bold
-        </button>
-        <button
-          onMouseDown={(event) => {
-            event.preventDefault();
-            CustomEditor.toggleCodeBlock(editor);
-          }}
-        >
-          Code Block
-        </button>
-      </div>
-      <Editable
-        renderElement={renderElement}
-        renderLeaf={renderLeaf}
-        onKeyDown={(event) => {
-          if (!event.ctrlKey) {
-            return;
-          }
-
-          switch (event.key) {
-            case "`": {
-              event.preventDefault();
-              CustomEditor.toggleCodeBlock(editor);
-              break;
-            }
-
-            case "b": {
-              event.preventDefault();
-              CustomEditor.toggleBoldMark(editor);
-              break;
-            }
-          }
-        }}
-      />
+      <Editable />
     </Slate>
-  );
-};
-
-const CodeElement = (props: any) => {
-  return (
-    <pre {...props.attributes}>
-      <code>{props.children}</code>
-    </pre>
-  );
-};
-
-const DefaultElement = (props: any) => {
-  return <p {...props.attributes}>{props.children}</p>;
-};
-
-const Leaf = (props: any) => {
-  return (
-    <span
-      {...props.attributes}
-      style={{ fontWeight: props.leaf.bold ? "bold" : "normal" }}
-    >
-      {props.children}
-    </span>
   );
 };
