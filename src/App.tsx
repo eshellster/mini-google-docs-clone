96f2a7f338902ad23b3c95fc4cebe57ea15386c1
eshell <eshellster@gmail.com>
2020-11-20 19:28:53 +0900

    초기 기본 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,15 @@
-import React, { useMemo, useState } from "react";
-import './App.css';
-import { createEditor,Node } from 'slate'
-import { Slate, Editable, withReact } from 'slate-react'
-
+import React from "react";
+import "./App.css";
+import { SyncingEditor } from "./SyncingEditor";
 
 const App = () => {
-  const editor = useMemo(() => withReact(createEditor()), [])
-  // Add the initial value when setting up our state.
-  const [value, setValue] = useState<Node[]>([
-    {
-      type: 'paragraph',
-      children: [{ text: 'A line of text in a paragraph.' }],
-    },
-  ])
-
   return (
-    <Slate editor={editor} value={value} onChange={newValue => setValue(newValue)}>
-     <Editable
-        onKeyDown={event => {
-          if (event.key === '&') {
-            // Prevent the ampersand character from being inserted.
-            event.preventDefault()
-            // Execute the `insertText` method when the event occurs.
-            editor.insertText("and")
-          }
-        }}
-      />
-    </Slate>
-  )
-}
+    <div>
+      <SyncingEditor />
+      <SyncingEditor />
+      <SyncingEditor />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/SyncingEditor.tsx
@@ -0,0 +1,30 @@
+import React, { useMemo, useState } from "react";
+import { createEditor, Node } from "slate";
+import { Slate, Editable, withReact } from "slate-react";
+
+export const SyncingEditor = () => {
+  // 랜더링간에 변경되지 않는 Slate 편집기 개체를 만듭니다.
+  const editor = useMemo(() => withReact(createEditor()), []);
+  // 편집기의 값에 대한 상태를 추적합니다.
+  // 상태를 설정할 때 초기 값을 추가합니다.
+  const [value, setValue] = useState<Node[]>([
+    {
+      type: "paragraph",
+      children: [{ text: "A line of text in a paragraph." }],
+    },
+  ]);
+
+  //슬레이트 컨텍스트를 랜더링합니다.
+  return (
+    //컨텍스트 내에 편집 가능한 구성 요소를 추가합니다.
+    <Slate
+      editor={editor}
+      value={value}
+      onChange={(newValue) => {
+        setValue(newValue);
+      }}
+    >
+      <Editable />
+    </Slate>
+  );
+};
