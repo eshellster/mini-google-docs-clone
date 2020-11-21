68724c1259bfdb5685e54a2f7ee006dcd199dfdb
eshell <eshellster@gmail.com>
2020-11-21 22:14:28 +0900

    메뉴로 속성변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import HoveringMenuExample from "./HoveringMenuExample";
 import { SyncingEditor } from "./SyncingEditor";
 
 const App = () => {
   return (
     <div>
       <SyncingEditor />
+      <HoveringMenuExample />
     </div>
   );
 };
