ff27eafb70588abeae2033b431f6c0895534eac7
eshell <eshellster@gmail.com>
2020-11-28 21:28:33 +0900

    Question

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import "./App.css";
 // import MentionExample from "./editors/mentions";
 // import HoveringMenuExample from "./editors/HoveringMenuExample";
 // import LinkExample from "./editors/Links";
-// import Puzzle from "./editors/Puzzle";
-import RichText from "./editors/RichtextWithPuzzle";
+import Puzzle from "./editors/Puzzle";
+import Question from "./editors/Question";
+// import RichText from "./editors/RichtextWithPuzzle";
 // import RichTextExample from "./editors/Richtext";
 // import { SyncingEditor } from "./editors/SyncingEditor";
 
@@ -21,10 +22,14 @@ const App = () => {
         <hr style={{ width: "100%", color: "white" }} />
         <LinkExample />
         <br /> */}
-        {/* <h2>Puzzle</h2>
+        <h2>Question</h2>
+        <hr style={{ width: "100%", color: "white" }} />
+        <Question />
+        <br />
+        <h2>Puzzle</h2>
         <hr style={{ width: "100%", color: "white" }} />
         <Puzzle />
-        <br /> */}
+        <br />
         {/* <h2>Local Saved Editor</h2>
         <hr style={{ width: "100%", color: "white" }} />
         <SyncingEditor />
@@ -33,10 +38,10 @@ const App = () => {
         <hr style={{ width: "100%", color: "white" }} />
         <HoveringMenuExample />
         <br /> */}
-        <h2>Rich Editor</h2>
+        {/* <h2>Rich Editor</h2>
         <hr style={{ width: "100%", color: "white" }} />
         <RichText />
-        <hr style={{ width: "100%", color: "white" }} />
+        <hr style={{ width: "100%", color: "white" }} /> */}
       </div>
     </div>
   );
--- src/editors/Question.tsx
+++ src/editors/Question.tsx
@@ -13,7 +13,7 @@ import { withHistory } from "slate-history";
 import { Button, Toolbar } from "../components/Components";
 import Icon from "../Icon";
 
-const Puzzle = () => {
+const Question = () => {
   const [value, setValue] = useState<Node[]>(initialValue);
 
   const editor = useMemo(
@@ -22,7 +22,7 @@ const Puzzle = () => {
   );
 
   useEffect(() => {
-    // console.log("value:", value);
+    console.log("value:", value);
   }, [value]);
 
   return (
@@ -52,7 +52,7 @@ const withPuzzles = (editor: any) => {
 };
 
 // 퍼즐 추가
-const insertPuzz = (editor: any, answer: any, guide: any) => {
+const insertPuzz = (editor: any, answer: any[], guide: any) => {
   const puzzle = { type: "puzzle", answer, guide, children: [{ text: "" }] };
   Transforms.insertNodes(editor, puzzle);
 };
@@ -76,22 +76,25 @@ const Element = (props: any) => {
 const PuzzleElement = ({ attributes, children, element }: any) => {
   const selected = useSelected();
   const focused = useFocused();
+  const emptyText = " ";
   return (
     <span
       {...attributes}
       contentEditable={false}
       style={{
+        textAlign: "center",
+        minWidth: "80px",
         padding: "3px 3px 2px",
         margin: "0 1px",
         verticalAlign: "baseline",
-        borderRadius: "4px",
-        backgroundColor: "#007AFF",
+        border: "solid 4px #007AFF",
+        display: "inline-block",
         fontSize: "0.9em",
-        color: "white",
         boxShadow: selected && focused ? "0 0 0 2px #B4D5FF" : "none",
       }}
     >
-      {element.guide}
+      {emptyText}
+      {/* {element.guide} */}
       {/* {element.answer} */}
       {children}
     </span>
@@ -102,16 +105,21 @@ const initialValue = [
   {
     children: [
       {
-        text: "In addition to block nodes, you can create inline nodes, like ",
+        text: "신라의 노래를 불러요",
       },
+    ],
+  },
+  {
+    children: [
       {
-        type: "puzzle",
-        answer: ["I am a boy"],
-        guide: "나는 소년이다",
-        children: [{ text: "" }],
+        text: "우리글이 없어서 한자의 음과 뜻을 빌려서 노랫말을 지었지요.",
       },
+    ],
+  },
+  {
+    children: [
       {
-        text: "!",
+        text: "노랫말은 주로 자신의 소원이나 귀신을 쫓는 내용이 많았어요.",
       },
     ],
   },
@@ -119,13 +127,30 @@ const initialValue = [
     children: [
       {
         text:
-          "This example shows hyperlinks in action. It features two ways to add links. You can either add a link via the toolbar icon above, or if you want in on a little secret, copy a URL to your keyboard and paste it while a range of text is selected.",
+          "널리 알려진 향가 중에 <서동요>라는 노래가 있어요. 백제에 사는 서동이 신라에 사는 선화 공주의 사랑을 얻기 위해 ",
+      },
+      {
+        type: "puzzle",
+        answer: ["서동요", "가요"],
+        guide: [],
+        children: [{ text: "" }],
+      },
+      {
+        text: "라는 노래를 지어 아이들에게 부르게 했대요. 함께 들어 볼까요?",
+      },
+    ],
+  },
+  {
+    children: [
+      {
+        text:
+          "[네이버 지식백과] 이제 어엿한 나라가 되었어요 (그림으로 보는 한국사, 2012.11.30., 황은희, 이명애, 역사와 사회과를 연구하는 초등 교사 모임(역사초모))",
       },
     ],
   },
 ];
 
-export default Puzzle;
+export default Question;
 
 const togglePuzzle = ({ editor, target, setTarget }: any) => {
   const isActive = isPuzzleActive(editor);
@@ -135,14 +160,14 @@ const togglePuzzle = ({ editor, target, setTarget }: any) => {
     const [start] = Range.edges(selection);
     const path = start.path;
 
-    const org = editor.children[path[0]].children[path[1]].answer;
+    const org = editor.children[path[0]].children[path[1]].answer[0];
 
     Transforms.delete(editor);
 
     editor.insertText(org);
   } else {
     const originalText = window.getSelection()?.toString();
-    const answer = originalText;
+    const answer = [originalText];
     if (selection && !Range.isCollapsed(selection)) {
       const [start, end] = Range.edges(selection);
       // console.log("start end Range: ", start, end);
@@ -153,11 +178,11 @@ const togglePuzzle = ({ editor, target, setTarget }: any) => {
         Transforms.select(editor, target);
       }
 
-      const guide = window.prompt(`"${originalText}"의 해석`);
+      const guide = [window.prompt(`"${originalText}"의 해석`)];
       if (!guide) return;
       //두번 입력방지와 강제로 커서를 위치시킨다. 현재는 필요가 없지만 나중에 활용
       //   editor.insertText("");
-      insertPuzz(editor, answer, guide);
+      insertPuzz(editor, answer, " ");
       //   console.log("answer, guide: ", answer, guide);
 
       setTarget(null);
