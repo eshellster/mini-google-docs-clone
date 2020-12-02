8ddce0f092bcdb52c9de300b5e5efbae83e8ee5b
eshell <eshellster@gmail.com>
2020-12-03 01:13:55 +0900

    퍼즐 중복작동 제거

--- src/editors/Puzzle.tsx
+++ src/editors/Puzzle.tsx
@@ -128,41 +128,68 @@ const initialValue = [
 
 export default Puzzle;
 
-const togglePuzzle = ({ editor, target, setTarget }: any) => {
-  const isActive = isPuzzleActive(editor);
+// const togglePuzzle = ({ editor, target, setTarget }: any) => {
+//   const isActive = isPuzzleActive(editor);
+//   const { selection } = editor;
+//   console.log(selection);
+
+//   if (isActive) {
+//     const [start] = Range.edges(selection);
+//     const path = start.path;
+
+//     const org = editor.children[path[0]].children[path[1]].answer;
+
+//     Transforms.delete(editor);
+
+//     editor.insertText(org);
+//   } else {
+//     const originalText = window.getSelection()?.toString();
+//     const answer = originalText;
+//     if (selection && !Range.isCollapsed(selection)) {
+//       const [start, end] = Range.edges(selection);
+//       // console.log("start end Range: ", start, end);
+//       const orgRange = start && end && Editor.range(editor, start, end);
+
+//       setTarget(orgRange);
+//       if (target) {
+//         Transforms.select(editor, target);
+//       }
+
+//       const guide = window.prompt(`"${originalText}"의 해석`);
+//       if (!guide) return;
+//       //두번 입력방지와 강제로 커서를 위치시킨다. 현재는 필요가 없지만 나중에 활용
+//       //   editor.insertText("");
+//       insertPuzz(editor, answer, guide);
+//       //   console.log("answer, guide: ", answer, guide);
+
+//       setTarget(null);
+//     }
+//   }
+// };
+
+const MakePuzzle = ({ editor, target, setTarget }: any) => {
   const { selection } = editor;
+  console.log(selection);
+  const originalText = window.getSelection()?.toString();
+  const answer = originalText;
+  if (selection && !Range.isCollapsed(selection)) {
+    const [start, end] = Range.edges(selection);
+    // console.log("start end Range: ", start, end);
+    const orgRange = start && end && Editor.range(editor, start, end);
 
-  if (isActive) {
-    const [start] = Range.edges(selection);
-    const path = start.path;
-
-    const org = editor.children[path[0]].children[path[1]].answer;
-
-    Transforms.delete(editor);
-
-    editor.insertText(org);
-  } else {
-    const originalText = window.getSelection()?.toString();
-    const answer = originalText;
-    if (selection && !Range.isCollapsed(selection)) {
-      const [start, end] = Range.edges(selection);
-      // console.log("start end Range: ", start, end);
-      const orgRange = start && end && Editor.range(editor, start, end);
-
-      setTarget(orgRange);
-      if (target) {
-        Transforms.select(editor, target);
-      }
-
-      const guide = window.prompt(`"${originalText}"의 해석`);
-      if (!guide) return;
-      //두번 입력방지와 강제로 커서를 위치시킨다. 현재는 필요가 없지만 나중에 활용
-      //   editor.insertText("");
-      insertPuzz(editor, answer, guide);
-      //   console.log("answer, guide: ", answer, guide);
-
-      setTarget(null);
+    setTarget(orgRange);
+    if (target) {
+      Transforms.select(editor, target);
     }
+
+    const guide = window.prompt(`"${originalText}"의 해석`);
+    if (!guide) return;
+    //두번 입력방지와 강제로 커서를 위치시킨다. 현재는 필요가 없지만 나중에 활용
+    //   editor.insertText("");
+    insertPuzz(editor, answer, guide);
+    //   console.log("answer, guide: ", answer, guide);
+
+    setTarget(null);
   }
 };
 
@@ -170,6 +197,9 @@ const MakePuzzleButton = () => {
   const editor = useSlate();
   // 선택된 문자열의 시작과 끝 지점 값
   const [target, setTarget] = useState<Range | null>();
+
+  console.log(isPuzzleActive(editor));
+  const isActive = isPuzzleActive(editor);
 
   // 선택된 에디터를 가져온다.
 
@@ -180,12 +210,18 @@ const MakePuzzleButton = () => {
       active={isPuzzleActive(editor)}
       onMouseDown={(event: any) => {
         event.preventDefault();
-
-        togglePuzzle({ editor, target, setTarget });
+        //퍼즐이 포함 되어있으면 작동 안함
+        if (!isActive) {
+          MakePuzzle({ editor, target, setTarget });
+        }
         // 선택한 문장을 answer변수에 저장한다.
       }}
     >
-      <Icon icon="format_Puzzle" size={20} />
+      <Icon
+        icon="format_Puzzle"
+        size={20}
+        color={isActive ? "gray" : "black"}
+      />
     </Button>
   );
 };
